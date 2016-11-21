3ae0831126e9ac7fd3c4ea36ba5b914a01637b06
Ihor Reshetnov <ihor.reshetnov@get.no>
2016-11-21 08:45:26 +0100

    Add property-resolution to test database context.

--- src/test/java/ua/sars/inc/ofm/OfferAdjustmentServiceTest.java
+++ src/test/java/ua/sars/inc/ofm/OfferAdjustmentServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -51,6 +52,7 @@ public class OfferAdjustmentServiceTest {
 
     @Configuration
     @Import(DatabaseConfig.class)
+    @PropertySource(value = "classpath:/application.properties")
     static class AppConfig {
 
         @Value("${sarsdb.test.url}")
